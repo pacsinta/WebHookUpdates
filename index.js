c466b2c7de91afccffbb9cf0e72715d3611c29cc
Patrik Csikos <pacsinta@gmail.com>
2022-01-14 16:53:26 +0100

    test post request

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const https =require('https');
+require('dotenv').config();
 
 const PORT = 1212;
 
 
 const app = express();
+
+app.use(express.json({ limit: "1kb" }));
+
+app.post("/WebHookUpdates", (req, res)=>{
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/cstcompany.ddns.net/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(httpsPORT, () => {
+	console.log(`GithubWebhook is running!`);
+    console.log(`Port: ${PORT}`);
+});

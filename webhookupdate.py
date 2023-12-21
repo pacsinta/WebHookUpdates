c11daee4f8cb17f3d1a7453be2146da2e4ac02a2
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2023-12-21 23:21:39 +0100

    Read basic yaml config in python

---
+++ webhookupdate.py
@@ -0,0 +1,35 @@
+import yaml
+from flask import Flask, request, Response
+
+# Read configuration
+with open("config.yaml", "r") as yamlfile:
+    cfg = yaml.load(yamlfile, Loader=yaml.SafeLoader)
+
+
+class WebhookUpdate:
+    port = 8080
+    host = "127.0.0.1"
+    url = "/webhook"
+
+    def validateConfig(self, cfgKey):
+        if(cfgKey in cfg and cfg[cfgKey] != ""):
+            return True
+        else:
+            return False
+
+    def __init__(self, cfg=None):
+        if(self.validateConfig("port")):
+            self.port = cfg["port"]
+            print("port: " + str(self.port))
+
+        if(self.validateConfig("host")):
+            self.host = cfg["host"]
+            print("host: " + str(self.host))
+
+        if(self.validateConfig("url")):
+            self.url = cfg["url"]
+            print("url: " + str(self.url))
+        
+        
+webhooks = WebhookUpdate(cfg)
+app = Flask(__name__)

81c45239051fc86a898db917d2485716c58d06a5
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2023-12-22 20:24:58 +0100

    Add basic route

--- webhookupdate.py
+++ webhookupdate.py
@@ -33,3 +33,11 @@ class WebhookUpdate:
         
 webhooks = WebhookUpdate(cfg)
 app = Flask(__name__)
+
+@app.route(webhooks.port, methods=['POST'])
+def respond():
+    print(request.json);
+    return Response(status=200)
+
+if __name__ == '__main__':
+    app.run(port=webhooks.port)

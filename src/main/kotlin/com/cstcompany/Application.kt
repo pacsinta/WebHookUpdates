c62b12f38c305089d3ca9355a82fa345d3d6087b
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2023-12-24 09:29:03 +0100

    Init Ktor project

---
+++ src/main/kotlin/com/cstcompany/Application.kt
@@ -0,0 +1,13 @@
+package com.cstcompany
+
+import com.cstcompany.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
